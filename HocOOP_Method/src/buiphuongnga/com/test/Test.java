f8ca8a8c3fa5aad95939695979f939ef79da84c4
zzz <zzz@DESKTOP-MVLU8FM>
2020-02-23 17:50:42 +0700

    Method

---
+++ HocOOP_Method/src/buiphuongnga/com/test/Test.java
@@ -0,0 +1,15 @@
+package buiphuongnga.com.test;
+
+import buiphuongnga.com.medule.TamGiac;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TamGiac tg1=new TamGiac(8,12,15);
+		System.out.println("Chu vi = "+tg1.tinhChuVi());
+		System.out.println("Diện tích = "+tg1.tinhDienTich());
+
+	}
+
+}
